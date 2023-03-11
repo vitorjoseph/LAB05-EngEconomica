0579705d9c851290ffca7ebb6fcd1dfdf5f4f263
Vítor José <83792704+vitorjoseph@users.noreply.github.com>
2023-03-11 16:56:33 -0300

    Add files via upload

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //Imprimir uma string informando o nome
+        System.out.println("Olá, meu nome é Vítor!!!");
+        //Imprimir uma string informando o curso
+        System.out.println("Estamos cursando a matéria de Engenharia Econômica para Software");
+        
+    }
+}
