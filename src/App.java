5f54ade4dd2104d6818c7337499fc92c93094a52
Vítor José <83792704+vitorjoseph@users.noreply.github.com>
2023-03-11 21:11:16 -0300

    Sort

--- src/App.java
+++ src/App.java
@@ -1,9 +1,25 @@
 public class App {
     public static void main(String[] args) throws Exception {
         //Imprimir uma string informando o nome
-        System.out.println("Olá, meu nome é Vítor!!!");
+        System.
+        out.
+        println
+        ("Olá, meu nome é Vítor!!!");
+
+
         //Imprimir uma string informando o curso
-        System.out.println("Estamos cursando a matéria de Engenharia Econômica para Software");
+        System.
+        out.
+        println
+        ("Estamos cursando a matéria de Engenharia Econômica para Software");
+        //Nota do aluno na matéria
+        nota();
         
     }
-}
+
+    public static int nota() 
+    {
+        int nota = 62;
+        return nota;  
+    }
+    }
